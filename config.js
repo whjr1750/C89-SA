3254e5ebabfade980b0d8ac0ad9c8f01b9ff114d
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2022-05-31 11:41:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXdGQCSpI-7zZLXnyU7r7h4JzHndudcKU",
+  authDomain: "storytellin-app.firebaseapp.com",
+  databaseURL: "https://storytellin-app-default-rtdb.firebaseio.com",
+  projectId: "storytellin-app",
+  storageBucket: "storytellin-app.appspot.com",
+  messagingSenderId: "569785301844",
+  appId: "1:569785301844:web:12f426488b06d9f67de6c1"
+};
